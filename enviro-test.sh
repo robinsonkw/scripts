547a8587b51c31f3b4948d9a8cf56da9be0f721a
Kevin Robinson <kwr@lanl.gov>
2020-08-10 14:01:07 -0600

    file: working file changes

--- enviro-test.sh
+++ enviro-test.sh
@@ -5,5 +5,13 @@ MACOS=$(echo $OSTYPE | grep 'darwin')
 UBUNTU=$(env | grep 'Ubuntu')
 echo $MACOS
 echo $UBUNTU
+
+if [ ! $MACOS=="" ];
+then
+    echo "$MACOS means this is a Mac"
+else
+    echo "The Mac OS environment variable is null."
+fi
+
 exit
 
