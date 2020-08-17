b29aa7e6ca6bbab35d7f558028f323c616fe174c
Kevin Robinson <kevin@yourtimeclinic.com>
2020-08-17 12:45:42 -0600

    file: updates

--- patch-download.sh
+++ patch-download.sh
@@ -10,12 +10,13 @@ the openEMR patch for the selected version.
 WEB_PATH='https://www.open-emr.org/patch/';
 VERSION='5-0-2';
 PATCH='3';
+read -n "Enter file path to place file: " PATH
 
 read -n "This will download openEMR version $VERSION
 with patch $PATCH. Ok? [yes/No]" confirm
 
 case
-    
+exit    
 esac
 # https://www.open-emr.org/patch/5-0-2-Patch-2.zip
 
