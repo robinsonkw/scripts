8f300fbcfaf299d50f575bacd954811b0a949dea
Kevin Robinson <kevin@yourtimeclinic.com>
2020-08-17 13:14:12 -0600

    file: add case statement and wget language

--- patch-download.sh
+++ patch-download.sh
@@ -7,16 +7,21 @@ echo "
 This will download the latest version of 
 the openEMR patch for the selected version.
 "
-WEB_PATH='https://www.open-emr.org/patch/';
-VERSION='5-0-2';
-PATCH='3';
-read -n "Enter file path to place file: " PATH
+WEB_PATH='https://www.open-emr.org/patch/'
+VERSION='5-0-2'
+PATCH='3'
 
-read -n "This will download openEMR version $VERSION
-with patch $PATCH. Ok? [yes/No]" confirm
+printf "Enter file path to place file: "
+read PATH
+printf "This will download openEMR version $VERSION
+with patch $PATCH. Ok? [yes/No]"
+read confirm
 
-case
-exit    
+case $confirm in
+    y*|Y*|"") wget -O $PATH/$VERSION'-Patch-'$PATCH'.zip' ${WEB_PATH}${VERSION}'-Patch-'$PATCH'.zip' ;;
+    n*|N*) echo "Patch download skipped."; return ;;
+    *) echo "Invalid choice. Patch download skipped."; return ;;
 esac
+
 # https://www.open-emr.org/patch/5-0-2-Patch-2.zip
 
