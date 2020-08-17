6026fa8ce720c4110c5a9c38a8e5c1ebd34f663f
Kevin Robinson <kevin@yourtimeclinic.com>
2020-08-17 12:33:19 -0600

    file: creating patch download script for openEMR

---
+++ patch-download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "
+     ###########################
+     #  openEMR Patch download #
+     ###########################
+
+This will download the latest version of 
+the openEMR patch for the selected version.
+"
+WEB_PATH='https://www.open-emr.org/patch/';
+VERSION='5-0-2';
+PATCH='3';
+
+read -n "This will download openEMR version $VERSION
+with patch $PATCH. Ok? [yes/No]" confirm
+
+case
+    
+esac
+# https://www.open-emr.org/patch/5-0-2-Patch-2.zip
+
