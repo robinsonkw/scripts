c64b14a36f8fcc15343148451bab803e387b1f96
Kevin Robinson <kwr@lanl.gov>
2020-09-10 12:45:52 -0600

    updating drives.sh script

--- drives.sh
+++ drives.sh
@@ -4,26 +4,50 @@
 #/users/isso/DriveInventory
 #Drive_inventory_DATE_TIME
 today=$(date +%Y%m%d)
-#exec > /users/isso/DriveInventory/$today-drives
+yesterday=$(date --date "Yesterday" "+%Y%m%d")
+DDNPATH='/users/isso/DriveInventory/DDN/status/'
+DDNFILE1='status-sys01'
+DDNFILE2='status-sys02'
+IDMGRPATH='/users/isso/host-based-disks-inventory/'
+IDMGRFILE='disksInventory.log.idmgr.*'
+CREMPATH='/archive/isso'
+WICKERSHAM='wickershams_disk_inv'
+SAM='sam-i-am_disk_inv'
+STORAGE='storagens2_disk_inv'
+NETAPP='netapp_disk_inv'
+DSM='dsmservns3_disk_inv'
+INVENTORYPATH='/users/isso/inventory/'
+INVENTORYARCHIVE='/users/isso/inventory/archive/'
 #filepath='/users/isso'
 #location=$filepath'/DriveInventory'
 #filename='drive_inventory'
-#datetime=$(date +%H:%M:%S)
+datetime=$(date +%H:%M:%S)
 #current_file=$(ls -tr | tail -n 1)
 #this_thursday=`date --date "Thursday" "+%m_%d_%Y"`
 #last_thursday=`date --date "last Thursday" "+%m_%d_%Y"`
 #old_file=`find $location -name ${filename}_${last_thursday}"*"`
 #new_file=`find $location -name ${filename}_${this_thursday}"*"`
+copies() {
 
+    cp ${DDNPATH}${DDNFILE1}    ${INVENTORYPATH}${DDNFILE1}-$yesterday
+    cp ${DDNPATH}${DDNFILE2}    ${INVENTORYPATH}${DDNFILE2}-$yesterday
+    cp ${IDMGRPATH}${IDMGRFILE} ${INVENTORYPATH}${IDMGRFILE}-$yesterday
+    cp ${CREMPATH}${WICKERSHAM} ${INVENTORYPATH}${WICKERSHAM}-$yeesterday
+    cp ${CREMPATH}${SAM}        ${INVENTORYPATH}${SAM}-$yesterday
+    cp ${CREMPATH}${STORAGE}    ${INVENTORYPATH}${STORAGE}-$yesterday
+    cp ${CREMPATH}${NETAPP}     ${INVENTORYPATH}${NETAPP}-$yesterday
+    cp ${CREMPATH}${DSM}        ${INVENTORYPATH}${DSM}-$yesterday    
+}
+ddn_compare() {
 
+    if [ -e ${INVENTORYPATH}${DDNFILE1} ]; then
+        
+
+}
+
+header(){
 echo "
 Script compares the disk report for drive inventories daily to track changes to
 drive and tape inventories.  This script was last run on $yesterday at $datetime.
-
-If nothing follows this section then the report found no discrepancies.
-*****************************************************************************
-$old_file
-$new_file
-
 "
-diff -i $old_file $new_file
+}
