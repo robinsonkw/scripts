1f5da3e10057eaba99cea166c4abcb6b5200cb30
Kevin Robinson <kevin@yourtimeclinic.com>
2020-09-21 10:35:29 -0600

    update files

--- columns.sh
+++ columns.sh
@@ -7,10 +7,23 @@ count=$(head -n 1 $file | awk '{ print NF; exit}')
 h=$(grep -n "-" $file | awk -F  ':' '{ print $1 }')
 (( h=h+1 ))
 
+function printcolumn() {
 if [[ "$count" == 3 ]]
     then
-    tail -n +$h $file | awk -v col=$count '{ print $col }'
+    text=$(tail -n +$h $file | awk -v col=$count '{ print $col }')
     else
     echo count does not equal.
 fi
+}
 
+function columntest() {
+    if [ -z "$text" ]
+    then
+        echo columnb is null
+    else
+        echo $text
+    fi
+}
+
+printcolumn
+columntest
