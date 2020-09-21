866eedc293da552affcd391a240378dbd878c9a8
Kevin Robinson <kevin@yourtimeclinic.com>
2020-09-21 08:36:42 -0600

    script updates

---
+++ columns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Test script to determine if data from a column is present, if not go to next column
+
+file='test.info'
+count="head -n 1 $file | awk '{ print NF; exit}'"
+if [ -z $count==3 ]
+    then
+    tail -n +2 $file | awk '{ print $($count) }'
+    else
+    echo count does not equal.
+fi
+
