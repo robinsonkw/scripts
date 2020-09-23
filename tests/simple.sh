8c9bdd4b4f88b31a3ca313f0b0cffa326594377e
Kevin Robinson <kwr@lanl.gov>
2020-09-23 11:33:11 -0600

    updates

--- tests/simple.sh
+++ tests/simple.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 paths=$(awk -F ',' '{ print $1 }' filepaths)
 files=$(awk -F ',' '{ print $2 }' filepaths)
+col=$(awk -F ',' '{print $3}' filepaths)
 
-while read files; do
-    printf "$paths"
-    printf "$files"
-    cat $paths/$files
-
+for i in $(cat filepaths);
+do
+	location=$(awk -F "," '{print $1"/"$2}' $i)
+	column=$(awk -F "," '{print $3}' $i)
+	echo $location
+	echo $column
 done
-exit
 
 
