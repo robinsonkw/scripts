8f035118985cab40296cefa692de105fb731365c
343538 <kwr@lanl.gov>
2020-09-29 08:57:11 -0600

    added new simple script file

--- tests/simple.sh
+++ tests/simple.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-paths=$(awk -F ',' '{ print $1 }' filepaths)
-files=$(awk -F ',' '{ print $2 }' filepaths)
-col=$(awk -F ',' '{print $3}' filepaths)
 
-function location {
-for i in $(cat filepaths);
-do
-
-	location=$(awk -F "," '{print $1 "/" $2}' $i)
-	column=$(awk -F "," '{print $3}' $i)
-	echo $location
-	echo $column
-
-	path=$(echo "$i" | awk -F ',' '{print $1}' "$i")
-    echo $path    
-done
-}
-for i in $(cat filepaths)
-do
-    echo $i
-
-done
-
-#location
+today=$(date +%Y%m%d)
 
