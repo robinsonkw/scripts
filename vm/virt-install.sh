49273bb46e659a8e5ebefafbec46b07f75db1c3b
Kevin Robinson <kwr@lanl.gov>
2021-08-16 06:24:22 -0600

    create: virt install script

---
+++ vm/virt-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+utilities_path=/var/lib/kvm/utilities/images
+services_path=/var/lib/kvm/services/images
+iso=/var/lib/kvm/services/images/ISO
+ks=/var/lib/kvm/scripts/ks
+
+virt-install \
+    --name ha-ansible \
+    --ram 4096 \
+    --disk path=${utilities_path}/ha-ansible.img,size=20 \
+    --vcpus 2 \                                                                                                                     
+    --os-type linux \
+    --os-variant rhel8.2 \
+    --network bridge=br0 \
+    --console pty,target_type=serial \
+        --initrd-inject ${ks}/remote-vm-ks.cfg \
+    --extra-args "ks=file:/ks.cfg",console=ttyS0 \
+    --location "${iso}"/rhel-8.2-x86_64-dvd.iso;
+
