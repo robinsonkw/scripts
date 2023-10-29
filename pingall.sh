937c8192b3c286ebdae7a31a4e3276303fe50c82
Kevin Robinson <robinsonkw@gmail.com>
2023-10-29 00:35:34 -0600

    made script actually work

--- pingall.sh
+++ pingall.sh
@@ -5,11 +5,11 @@ clear
 #ping=$(ping -c 1 )
 #up=$($ping $i | grep '1 packets received')
 
-while read $i
+while read -r i
 do
     up=$(ping -c 1 $i | grep '1 packets received')
     if [[ ! -z "$up" ]]
-    then echo "Host up"
-    else echo "Host down"
+    then echo "$i is up"
+    else echo "$i is down"
     fi
 done<hosts
