257a69482b988e09c97c8f684efdcb423952d9f2
Kevin Robinson <robinsonkw@gmail.com>
2023-10-29 00:25:35 -0600

    changed script based on testing

--- pingall.sh
+++ pingall.sh
@@ -2,18 +2,14 @@
 
 clear
 
-ping=$(ping -c 1 )
+#ping=$(ping -c 1 )
+#up=$($ping $i | grep '1 packets received')
 
-echo ping 192.168.1.1
-echo ping 192.168.1.2
-echo ping 192.168.1.3
-echo ping 192.168.1.4
-echo ping 192.168.1.6
-echo ping 192.168.1.8
-echo ping 192.168.1.10
-echo ping 192.168.1.11
-echo ping 192.168.1.15
-echo ping 192.168.1.100
-echo ping 192.168.1.202
-echo ping 192.168.1.205
-echo ping 192.168.1.212
+while read $i
+do
+    up=$(ping -c 1 $i | grep '1 packets received')
+    if [[ ! -z "$up" ]]
+    then echo "Host up"
+    else echo "Host down"
+    fi
+done<hosts
