b2cda7f0db871c11678a765ccc8a6aedc54f7aa9
Kevin Robinson <robinsonkw@gmail.com>
2023-10-28 23:42:05 -0600

    script to ping all network devices

---
+++ pingall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+
+ping=$(ping -c 1 )
+
+echo ping 192.168.1.1
+echo ping 192.168.1.2
+echo ping 192.168.1.3
+echo ping 192.168.1.4
+echo ping 192.168.1.6
+echo ping 192.168.1.8
+echo ping 192.168.1.10
+echo ping 192.168.1.11
+echo ping 192.168.1.15
+echo ping 192.168.1.100
+echo ping 192.168.1.202
+echo ping 192.168.1.205
+echo ping 192.168.1.212
