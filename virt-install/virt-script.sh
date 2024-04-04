3cd141a075c71f6e3be6cf7dec6bc6d20e56b306
Kevin Robinson <robinsonkw@gmail.com>
2024-04-04 16:02:53 -0600

    reorganizing files and directories

---
+++ virt-install/virt-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+disk_path=/var/lib/libvirt/images/
+isos=/mnt/zfs/images/
+
+virt-install \
+    --name alma \
+    --vcpus 2 \
+    --memory 4096 \
+    --location ${isos}AlmaLinux-9.3-x86_64-dvd.iso \
+    --disk path=${disk_path}alma.img,size=20 \
+    --network bridge=virtbr0 \
+    --graphics vnc,port='-1',listen=0.0.0.0 \
+#    --os-type linux \
+    --os-variant almalinux9 \
+    --console pty,target_type=serial \
+    --extra-args 'console=tty0 console=ttyS0,115200'
+   
+
