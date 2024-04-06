cd139564c4e96e8f29aafdce709b797e8c894bef
Kevin Robinson <robinsonkw@gmail.com>
2024-04-06 16:23:56 -0600

    completed script

--- VMs/virt-script.sh
+++ VMs/virt-script.sh
@@ -4,39 +4,31 @@
 # eventually will add OPSARGS and switches for VMs / containers
 
 ## Global variables
-disk_path=/var/lib/libvirt/images/ # location the VM image file is located
-iso_path=/mnt/zfs/images/ # ISO location for install
-ks_path=
-host_name=alma-vm
-disk_size=20G
-disk_format=qcow2
+disk_path=/var/lib/libvirt/images/  # location the VM image file is located
+iso_path=/mnt/zfs/images/           # ISO location for install
+ks_path=/mnt/zfs/kickstart/         # location of ks.cfg files
+host_name=alma-vm                   # host name of VM
+disk_size=20G                       # disk size of VM
+disk_format=qcow2                   # format of disk image file
 #isos=/mnt/zfs/isos/  # need to rename the folder eventually
-
-function qcow {
-## Test if the qcow2 file is created
-
-# qemu-img create -f ${disk_format} ${disk_path}${host_name}.${disk_format} ${disk_size}
-}
 
 function create {
 
 virt-install \
     --name ${host_name} \
     --vcpus 2 \
-    --memory 4096 \
+    --memory memory=4096,currentMemory=2048 \
     --location ${iso_path}AlmaLinux-9.3-x86_64-dvd.iso \
-    --disk ${disk_path}${host_name}.${disk_format},format=${disk_format} \
+    --disk path=${disk_path},size=${disk_size},format=${disk_format},cache=none,discard=unmap \
     --network bridge=virtbr0 \
     --graphics vnc,listen=0.0.0.0 --noautoconsole \
     --os-variant almalinux9 \
+    --tpm model='tpm-crb',type=emulator,version='2.0' \
+    --channel type=unix,target.type=virtio,target.name=org.qemu.guest_agent.0 \
     --console pty,target_type=serial \
     --initrd-inject ${ks_path}/${host_name}-ks.cfg \
-    --extra-args "ks=file:/ks.cfg",console=tty0,console=ttyS0
+    --extra-args "ks=file:/ks.cfg",'console=tty0 console=ttyS0,115200n8 --- console=tty0 console=ttyS0,115200n8'
+#    --boot uefi
 }
 
-function run_order {
-    qcow
-    create
-}
-
-run_order
+create
