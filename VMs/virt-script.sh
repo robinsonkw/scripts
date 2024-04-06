76a9ae10647fce7ca694b95d613b2928f1f25d26
Kevin Robinson <robinsonkw@gmail.com>
2024-04-06 16:06:14 -0600

    modifying the virt-install script with functions for order and logic functions

--- VMs/virt-script.sh
+++ VMs/virt-script.sh
@@ -3,19 +3,40 @@
 # script to generate virtual machines through KVM in linux
 # eventually will add OPSARGS and switches for VMs / containers
 
-# qemu-img create -f qcow2 /mnt/zfs/libvirt/images/alma.qcow2 20G
+## Global variables
+disk_path=/var/lib/libvirt/images/ # location the VM image file is located
+iso_path=/mnt/zfs/images/ # ISO location for install
+ks_path=
+host_name=alma-vm
+disk_size=20G
+disk_format=qcow2
+#isos=/mnt/zfs/isos/  # need to rename the folder eventually
 
-disk_path=/var/lib/libvirt/images/
-isos=/mnt/zfs/images/
+function qcow {
+## Test if the qcow2 file is created
+
+# qemu-img create -f ${disk_format} ${disk_path}${host_name}.${disk_format} ${disk_size}
+}
+
+function create {
 
 virt-install \
-    --name alma \
+    --name ${host_name} \
     --vcpus 2 \
     --memory 4096 \
-    --location ${isos}AlmaLinux-9.3-x86_64-dvd.iso \
-    --disk ${disk_path}alma.qcow2,format=qcow2 \
+    --location ${iso_path}AlmaLinux-9.3-x86_64-dvd.iso \
+    --disk ${disk_path}${host_name}.${disk_format},format=${disk_format} \
     --network bridge=virtbr0 \
-    --graphics vnc,,listen=0.0.0.0 --noautoconsole \
+    --graphics vnc,listen=0.0.0.0 --noautoconsole \
     --os-variant almalinux9 \
     --console pty,target_type=serial \
-    --extra-args 'console=tty0,115200 console=ttyS0,115200'
+    --initrd-inject ${ks_path}/${host_name}-ks.cfg \
+    --extra-args "ks=file:/ks.cfg",console=tty0,console=ttyS0
+}
+
+function run_order {
+    qcow
+    create
+}
+
+run_order
